95aa2517581aec1bad688ee32f5545b707642b26
Roman <35653846+Li-Roman@users.noreply.github.com>
2023-07-05 13:54:16 +0300

    add UIColor Extension

---
+++ WhatsDog_app/Extensions/UIColorExtexnsion.swift
@@ -0,0 +1,37 @@
+//
+//  UIColorExtexnsion.swift
+//  WhatsDog_app
+//
+//  Created by Роман Хилюк on 05.07.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    struct HairColor {
+        static var whiteColor        : UIColor { return UIColor.white }
+        static var whiteColorGR      : UIColor { return UIColor.white }
+        
+        static var blackColor        : UIColor { return UIColor.black }
+        static var blackColorGR      : UIColor { return UIColor.black }
+        
+        static var goldColor         : UIColor { return UIColor(red: 238/255, green: 232/255, blue: 170/255, alpha: 1) }
+        static var goldColorGR       : UIColor { return UIColor(red: 238/255, green: 232/255, blue: 170/255, alpha: 1) }
+        
+        static var lightBrown        : UIColor { return UIColor(red: 153/255, green: 121/255, blue: 80/255,  alpha: 1) }
+        static var lightBrownGR      : UIColor { return UIColor(red: 238/255, green: 232/255, blue: 170/255, alpha: 1) }
+        
+        static var redHairColor      : UIColor { return UIColor(red: 225/255, green: 125/255, blue: 23/255,  alpha: 1) }
+        static var redHairColorGR    : UIColor { return UIColor(red: 238/255, green: 232/255, blue: 170/255, alpha: 1) }
+        
+        static var brownColor        : UIColor { return UIColor(red: 124/255, green: 71/255,  blue: 0/255,   alpha: 1) }
+        static var brownColorGR      : UIColor { return UIColor(red: 238/255, green: 232/255, blue: 170/255, alpha: 1) }
+        
+        static var darkBrownColor    : UIColor { return UIColor(red: 72/255,  green: 38/255,  blue: 13/255,  alpha: 1) }
+        static var darkBrownColorGR  : UIColor { return UIColor(red: 238/255, green: 232/255, blue: 170/255, alpha: 1) }
+        
+        static var grayColor         : UIColor { return UIColor(red: 105/255, green: 105/255, blue: 105/255, alpha: 1) }
+        static var grayColorGR       : UIColor { return UIColor(red: 238/255, green: 232/255, blue: 170/255, alpha: 1) }
+    }
+}
+
